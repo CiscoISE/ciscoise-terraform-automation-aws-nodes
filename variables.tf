3dd12c06ccd1c3c2061430974b1535b2112cf6e5
bhadeshp <bhadeshp@cisco.com>
2023-08-17 10:20:06 +0530

    Updating region to Ohio(us-east-2)

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "ami_ids" {
 variable "aws_region" {
   description = "The AWS region where resources will be created."
   type        = string
-  default     = "us-east-1" # Change this to your desired default region
+  default     = "us-east-2" # Change this to your desired default region
 }
 
 
